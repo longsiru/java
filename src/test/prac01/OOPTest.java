6b3b0da5c631ead6a1f837e50e35527a1e154954
longsiru <sirulong89@gmail.com>
2022-11-22 20:15:45 +0900

    up

---
+++ src/test/prac01/OOPTest.java
@@ -0,0 +1,18 @@
+package test.prac01;
+/*
+ * 面向过程：procedure oriented programming,强调的是功能行为，考虑怎么做
+ * 面向对象：object oriented programming，强调具备了功能的对象，考虑谁来做
+ * 一、Java面对对象学习的三条主线
+ * 	1.Java类以及类的成员： 属性，方法，构造器； 代码块，内部类
+ * 	2.面对对象的三大特征：封装性(Encapsulation)， 继承性(Inheritance)， 多态性(Polymorphism)。(抽象性)
+ * 	3.其他关键字：this,super,static,final,abstract,interface,package,import等等
+ * 
+ * 二、面对对象的两个要素：
+ * 类和对象的核心概念：
+ * 	1.类是对一类事物的描述，是抽象的、概念上的定义。
+ * 			面对对象设计程序的重点是类的设计，类的设计其实就是成员的设计。
+ * 	2.对象是实际存在的该类事物的每个主体，因而也称为（instance）
+ */
+public class OOPTest {
+
+}
---
+++ src/test/prac01/PersonTest.java
@@ -0,0 +1,17 @@
+package test.prac01;
+/*
+ * 一、类的设计，其实就是设计类的成员。
+ * 
+ * 
+ * 
+ */
+public class PersonTest {
+
+}
+
+
+class Person{
+	//属性
+	
+	//方法
+}
